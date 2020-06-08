d5affd50fa8e5b0dfee56767868931fa022e8893
mohmd labeeb <labeebshanavas@gmail.com>
2020-06-08 07:24:48 +0530

    add mongodb database

--- app.js
+++ app.js
@@ -2,7 +2,34 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 // local date module to get the date 
-const date = require(__dirname +"/date.js");
+const date = require(__dirname + "/date.js");
+
+
+//-----------------------     MONGO DB   --------------------------------------------
+
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/todolist", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const itemSchema = new mongoose.Schema({
+    name: String
+});
+
+const item = mongoose.model('item', itemSchema);
+
+const workItem = mongoose.model('workItem', itemSchema);
+
+const item1 = new item({
+    name: "Welcome to to-do-list"
+});
+
+const item2 = new item({
+    name: "use + to add items"
+});
+
+
+//-----------------------     MONGO DB   --------------------------------------------
+
 
 const app = express();
 
@@ -12,40 +39,94 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-const items = ["bake", "cook"];
-const workItems=[];
+
+
+
 
 app.get("/", function (req, res) {
- const day = date.getDate();
-  res.render("index", { day: day, add: items });
+
+    item.find({}, function (err, items) {
+        if (items.length === 0) {
+            item.insertMany([item1, item2], function (err) {
+                if (err) {
+                    console.log("error");
+                }
+                else {
+                    console.log("success")
+                }
+            });
+            res.redirect("/");
+        }
+        else {
+            const day = date.getDate();
+            res.render("index", { day: day, add: items })
+        }
+
+
+    });
+
+
+    ;
 });
 
 app.post("/", function (req, res) {
 
-if(req.body.type === "work")
-{
-    const newItem = req.body.listAdd;
-    workItems.push(newItem);
-    res.redirect("/work")
-}
-else
-{
-    console.log(req.body);
-    const add = req.body.listAdd;
-    items.push(add);
-  
-    res.redirect("/");
-}
- 
+    if (req.body.type === "work") {
+        const newItem = req.body.listAdd;
+
+        const newData = new workItem({
+            name: newItem
+        });
+
+        newData.save();
+
+        res.redirect("/work")
+    }
+    else {
+        const add = req.body.listAdd;
+
+        const newitem = new item({
+            name: add
+        })
+
+        newitem.save();
+
+        res.redirect("/");
+    }
+
 });
 
-app.get("/work",function (req,res){
-    res.render("index", {day:"work", add:workItems});
+
+app.post("/delete", function (req, res) {
+    const itemId = req.body.checkbox;
+
+    item.findByIdAndDelete(itemId, function (err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("Deleted");
+        }
+    });
+res.redirect("/");
 });
 
-app.get("/about",function (req,res){
+app.get("/work", function (req, res) {
+    workItem.find({}, function (err, workitems) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log(workitems);
+            res.render("index", { day: "work", add: workitems });
+        }
+    })
+
+});
+
+app.get("/about", function (req, res) {
     res.render("about")
 });
 app.listen(5000, function () {
-  console.log("app is running");
+    console.log("app is running");
 });
